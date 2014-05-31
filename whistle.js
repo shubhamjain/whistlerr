7ef6a8e49794279fd09260e7a04b05a7de511169
Shubham Jain <shubham.jain.1@gmail.com>
2014-05-31 18:06:47 +0530

    Refractoring.

--- whistle.js
+++ whistle.js
@@ -1,34 +1,35 @@
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext = new AudioContext();
+function whistlerr(whistleCallback){	
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	var audioContext = new AudioContext();
 
-function getUserMedia(dictionary, callback) {
-	try {
-		navigator.getUserMedia = 
-		navigator.getUserMedia ||
-		navigator.webkitGetUserMedia ||
-		navigator.mozGetUserMedia;
-		navigator.getUserMedia(dictionary, callback);
-	} catch (e) {
-		alert('getUserMedia threw exception :' + e);
+	function getUserMedia(dictionary, callback) {
+		try {
+			navigator.getUserMedia = 
+			navigator.getUserMedia ||
+			navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia;
+			navigator.getUserMedia(dictionary, callback);
+		} catch (e) {
+			alert('getUserMedia threw exception :' + e);
+		}
 	}
-}
 
-function gotStream(stream) {
-	// Create an AudioNode from the stream.
-	var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+	function gotStream(stream) {
+		// Create an AudioNode from the stream.
+		var mediaStreamSource = audioContext.createMediaStreamSource(stream);
 
-	// Connect it to the destination.
-	window.analyser = audioContext.createAnalyser();
+		// Connect it to the destination.
+		window.analyser = audioContext.createAnalyser();
 
-	mediaStreamSource.connect( analyser );
-	whistleFinder();
-}
+		mediaStreamSource.connect( analyser );
+		whistleFinder();
+	}
 
-getUserMedia({audio: true}, gotStream);
+	getUserMedia({audio: true}, gotStream);
 
 
-/*** Whistle Detection code ***/
-(function(){		
+	/*** Whistle Detection code ***/
+	
 	var freqBinCount = 1024; //default
 	var freq_buf = new Uint8Array( freqBinCount ); //frequency domain data
 	var time_buf = new Uint8Array( freqBinCount ); //time domain data
@@ -38,19 +39,17 @@ getUserMedia({audio: true}, gotStream);
 
 	function whistleFinder()
 	{
+
 		analyser.getByteFrequencyData(freq_buf);
-
-		// For mean and standard deviation
-		sumX = 0;
-		sumX2 = 0;
-
-		// According to "Human Whistle Detection and Frequency Estimation" most human whistles 
-		// lie between 600 Hz to 5000 Hz. For buffer length 1024 and 22.05 Khz Max frequency, each element in array will correspond to ~22.533 Hz and hence the corresponding values, 29 and 232.
 
 		max_energy = 0;
 		prev_energy = 0;
 		peak_samples = 0;	// Too many peak samples is probably a noise.
 		max_energy_gap = 0; // We need to find the sharpest spike in sample
+
+
+		// According to "Human Whistle Detection and Frequency Estimation" most human whistles 
+		// lie between 600 Hz to 5000 Hz. For buffer length 1024 and 22.05 Khz Max frequency, each element in array will correspond to ~22.533 Hz and hence the corresponding values, 29 and 232.
 
 		for (i = 29; i < 232; i+= WINDOW_SIZE)
 		{
@@ -77,12 +76,11 @@ getUserMedia({audio: true}, gotStream);
 		// that many valid samples quantify only a single whistle.
 		if( max_energy_gap > 250 && peak_samples < 20 && max_energy > 1100 && non_positives > 30)
 		{
-			document.getElementById("log").innerHTML += "Whistle at: " + new Date() + "\n";
+			whistleCallback();
 			non_positives = 0;
 		} else {
 			non_positives++;
 		}
 		window.webkitRequestAnimationFrame(whistleFinder);
 	}
-	window.whistleFinder = whistleFinder;
-})();
+};
